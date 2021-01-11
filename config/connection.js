6d89eec7ad1c20501499913d89fb2e49c09706ad
highwolfx <seanchangx@gmail.com>
2021-01-10 21:42:17 -0800

    Altered connection.js for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,16 @@ var mysql = require("mysql");
 var config = require('./config.js');
 var connection = mysql.createConnection(config.databaseOptions);
 
-connection.connect(function(err) {
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
   console.log("connected as id " + connection.threadId);
-});
+  });
+}
 
 module.exports = connection;
