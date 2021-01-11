9af0bc71885683bf99b2273fe9d81d27553b33a0
highwolfx <seanchangx@gmail.com>
2021-01-10 21:34:17 -0800

    Added css and other button functionality to the app.

--- public/assets/burgers.js
+++ public/assets/burgers.js
@@ -1,33 +1,35 @@
 $(function() {
-    $(".change-eaten").on("click", function(event) {
-        var id = $(this).data("id");
-    
-        var devoured = {
-          devoured: true
-        };
-    
-        // Send the PUT request.
-        $.ajax("/api/burgers/" + id, {
-          type: "PUT",
-          data: devoured
-        }).then(
-          function() {
-            console.log("Changed to devoured, burger ID: ", id);
-            // Reload the page to get the updated list
-            location.reload();
-          }
-        );
-    });
-
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
+  $(".change-eaten").on("click", function(event) {
+      var id = $(this).data("id");
   
-      var newBurger = {
-        name: $("#burgername").val().trim(),
+      var devoured = {
+        devoured: true
       };
   
-      // Send the POST request.
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: devoured
+      }).then(
+        function() {
+          console.log("Changed to devoured, burger ID: ", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+  
+    var newBurger = {
+      name: $("#burgername").val().trim(),
+    };
+    
+    if (!newBurger.name.toString().replace(/\s/g, '').length){
+      $('#errorModal').modal('show');
+    } else {
       $.ajax("/api/burgers", {
         type: "POST",
         data: newBurger
@@ -38,6 +40,11 @@ $(function() {
           location.reload();
         }
       );
-    });
+    }
+  });
+
+  $(".close").on("click", function(event) {
+    $('#errorModal').modal('hide');
+  });
 });
   
