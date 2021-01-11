790290d46574d627d690e5a394ae92b02c278e5e
highwolfx <seanchangx@gmail.com>
2021-01-10 20:09:37 -0800

    Added devouring functionality and changing from one section to another

--- public/assets/burgers.js
+++ public/assets/burgers.js
@@ -1,9 +1,27 @@
 $(function() {
+    $(".change-eaten").on("click", function(event) {
+        var id = $(this).data("id");
+    
+        var devoured = {
+          devoured: true
+        };
+    
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: devoured
+        }).then(
+          function() {
+            console.log("Changed to devoured, burger ID: ", id);
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+    });
+
     $(".create-form").on("submit", function(event) {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
-
-      console.log("I've been clicked!");
   
       var newBurger = {
         name: $("#burgername").val().trim(),
