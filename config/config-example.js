29cb7eee47fd3f83d7a4103eb05c3e25917b0a08
highwolfx <seanchangx@gmail.com>
2021-01-10 18:11:33 -0800

    Added base orm file and a config example file

---
+++ config/config-example.js
@@ -0,0 +1,7 @@
+module.exports.databaseOptions = {
+    host     : 'localhost',
+    database : 'burgers_db',
+    user     : 'root',
+    password : 'password',
+    port     : '3306'
+};
---
+++ config/orm.js
@@ -0,0 +1,30 @@
+const { connect } = require("./connection.js");
+var connection = require("./connection.js");
+
+var orm = {
+    selectAll: function(cb){
+        var queryString = "SELECT * FROM burgers";
+        connnection.query(queryString, (err,result) => {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    insertOne: function(addBurger,cb){
+        var queryString = "INSERT INTO burgers VALUES ?";
+        connection.query(queryString, [addBurger, false], (err, result) => {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    updateOne: function(burgerID,cb){
+        var queryString = "UPDATE burgers SET devoured=true WHERE id=?";
+        connection.query(queryString, [burgerID], (err, result) => {
+            connection.query(queryString, [addBurger, false], (err, result) => {
+                if (err) throw err;
+                cb(result);
+            });
+        });
+    }
+};
+
+module.exports = orm;
