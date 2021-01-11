0da234229faa263ef30870c828d0027d7b54fc86
highwolfx <seanchangx@gmail.com>
2021-01-10 18:17:24 -0800

    Added models folder and file.

---
+++ models/burgers.js
@@ -0,0 +1,21 @@
+var orm = require('../config/orm.js');
+
+var burger = {
+    select: (cb) => {
+        orm.selectAll((res) => {
+            cb(res);
+        });
+    },
+    add: (addBurger, cb) => {
+        orm.insertOne(addBurger, (res) => {
+            cb(res);
+        });
+    },
+    update: (burgerID, cb) => {
+        orm.updateOne(burger, (res) => {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
