7c46df6b2a8c2cbc8032df13e1f984e1bb84a766
highwolfx <seanchangx@gmail.com>
2021-01-10 17:51:16 -0800

    Added database seed and schema files, gitignore, and node requirements

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(burger_name, devoured) VALUES ('Swiss Cheese Mushroom Burger', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('4x4 Burger', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Quarter Pounder Cheeseburger', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Heart Attack Cheeseburger', false);
